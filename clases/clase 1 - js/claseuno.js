01b5059634bb5dbd2d2427e3b5362b6018b5974c
Evelyn <beatlyn007@gmail.com>
2023-06-23 18:33:16 -0300

    new proyect ppt

---
+++ clases/clase 1 - js/claseuno.js
@@ -0,0 +1,30 @@
+/*const firstName=prompt('Cual es tu nombre?')
+const lastName=prompt('Cual es tu apellido?')
+
+alert ('Bienvenida! ' + firstName + ' ' + lastName)
+
+const age= prompt(`Hola, ${firstName}  ${lastName}, cual es tu edad?`)
+const result=alert(`${firstName} ${lastName} tienes ${age} y el año que viene tendras ${parseInt(age) + 1} `)*/
+
+
+/*const ageOfPerson=prompt('Que edad tienes?')
+
+const parsedAge = parseInt(ageOfPerson) //El parseInt convierte el string en un caracer numerico
+
+if(parsedAge > 18){
+    alert ('Puede pasaar!')
+}else if(parsedAge === 18){
+    alert ('Justitoo')
+}else{
+    alert('Acceso Denegado')
+}*/
+
+//Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+
+let nombre = prompt('Cual es tu nombre?')
+let apellido = prompt('Cual es tu apellido?')
+
+alert (`Hola ${nombre} ${apellido}, bienvenido al curso de programacion!`)
+
+let edad = prompt(`Hola ${nombre} ${apellido}, eres bienvenido al curso de programacion. Cual es tu edad?`)
+let result = alert(`${nombre} ${apellido} tiene ${edad} años y el proximo año tendra ${parseInt(edad)+ 1} años de edad`)
---
+++ proyecto/piedrapapeltijera/piedrapapeltijera.js
@@ -0,0 +1,13 @@
+let jugador=0
+
+jugador=prompt('Elige 1 para PIEDRA, 2 para PAPEL O 3 para TIJERA')
+
+if(jugador==1){
+    alert('Elegiste PIEDRAA')
+}else if(jugador==2){
+    alert('Elegiste PAPEEL')
+}else if(jugador==3){
+    alert('elegiste TIJERA')
+}else{
+    alert('MOVIMIENTO INVALIDO!')
+}
